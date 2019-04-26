0ff86d62cc3c59df1ed3a0b62f74ed36e2ee0070
Amanda Abate <amanda.abate@qsc.com>
2019-04-26 15:28:30 -0600

    clean push

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "Shure~Axient~AD4D/AD4Q Receiver",
-  Version = "1.0.1.10",
-  Id = "COMPILEDPLUGIN-Axient-1.0.1.10",
+  Version = "1.0.0.1",
+  Id = "COMPILEDPLUGIN-Axient-1.0.0.1",
   Author = "QSC",
   Description = "Shure AD4D/AD4Q v0.1 from VSCode"
 }
